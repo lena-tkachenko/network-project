17ba72aaa20ece26408f95328a63dbfc318a8df0
lena-tkachenko <lena.tkachenko.physic@gmail.com>
2020-11-18 13:30:02 +0200

    this is the first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="logoTitle">
+      <h1>Mind<span className="changeColor">y</span>.</h1>
+      <h6>small logo slogan</h6>
+    </div>
+  );
+}
+
+export default App;
